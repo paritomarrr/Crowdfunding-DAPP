7401d936c7a8b7ac7e09b05357d7c2c81e1b2d24
amateur-dev <me@dipeshsukhani.dev>
2022-06-28 18:35:38 +0800

    reformating

--- packages/web3/utils/constants.ts
+++ packages/web3/utils/constants.ts
@@ -1,31 +1,12 @@
-// read the deployments folder
-// find all chainIDs
-// create an object for each chainID and its deployed Address
-// eg
-
-import glob from "glob";
-import fs from "fs/promises";
-
-const addresses = {};
-
-glob('deployments/**/.chainId', function (err, files) {
-    if (err) {
-        console.log(err);
+export const DEPLOYMENTS = {
+    "31337" : {
+        "crowdfunding": {
+            address: "",
+            abi:""
+        },
+        "project":  {
+            address: "",
+            abi:""
+        } 
     }
-
-    files.forEach(async (e) => {
-        const chainId = await fs.readFile(e, { encoding: 'utf8' });
-        let crowdfundingAddress;
-        let projectAddress;
-        let path = e.replaceAll(".chainId","");
-        // try {
-        //     find the abi and address from the json file from the path        
-        // } catch (error) {
-            
-        // }        
-        
-    }
-        //open file and read the chainId
-    );
-});
-
+}
