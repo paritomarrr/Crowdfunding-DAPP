b862f9f20d9cd69f965bee6f286abd2c373a350a
amateur-dev <me@dipeshsukhani.dev>
2022-06-29 09:18:12 +0800

    WIP

--- packages/web3/deploy/01_deploy_Crowdfunding.ts
+++ packages/web3/deploy/01_deploy_Crowdfunding.ts
@@ -1,14 +1,17 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { fetchDeploymentDetails } from "../utils/fetchDeploymentDetails";
+import { CONSTANTS } from "../constants/constants.json";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { getNamedAccounts, deployments } = hre;
     const { deployer } = await getNamedAccounts();
     const { deploy } = deployments;
-    await deploy("Crowdfunding", {
+    const deployTx = await deploy("Crowdfunding", {
       from: deployer,
       log: true,
     });
+    CONSTANTS[await hre.getChainId()] = await fetchDeploymentDetails(deployTx);
 };
 
 func.tags = ["Crowdfunding"];
